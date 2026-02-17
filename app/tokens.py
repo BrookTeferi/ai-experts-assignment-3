4664edf50b9f9674f4f06278bc9ff23a7944a64b
BrookTeferi <brookteferi1q2w@gmail.com>
2026-02-17 19:14:25 +0300

    Fix formatting in README.md and enhance timezone handling in token_from_iso function in tokens.py

--- app/tokens.py
+++ app/tokens.py
@@ -22,7 +22,11 @@ class OAuth2Token:
 
 def token_from_iso(access_token: str, expires_at_iso: str) -> OAuth2Token:
     dt = isoparse(expires_at_iso)
+
     if dt.tzinfo is None:
         dt = dt.replace(tzinfo=timezone.utc)
+    else:
+        dt = dt.astimezone(timezone.utc)
+
     expires_at = int(dt.timestamp())
     return OAuth2Token(access_token=access_token, expires_at=expires_at)
